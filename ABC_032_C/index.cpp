c72fc7d323ffb93930bea5d5355719ab9e75664c
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-06-28 11:56:57 +0900

    solve ABC 032 c

---
+++ ABC_032_C/index.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(arg) std::cerr << #arg << ": " << (arg) << '\n'
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+int main() {
+	int N, K; cin >> N >> K;
+	vector<int> list(N);
+
+	rep(i, N) {
+		if (list[i] == 0) {
+			cout << N << endl;
+		}
+	}
+
+	int answer = 0;
+	int right = 0;
+	ll mul = 1;
+	for (int left=0; left<N; left++) {
+		while(right < N && mul * list[right] <= K) {
+			mul *= list[right];
+			right++;
+		}
+
+		answer = max(answer, right - left);
+		if (left == right) right++;
+		else mul /= list[left];
+
+	}
+	cout << answer << endl;
+	return 0;
+}
+
