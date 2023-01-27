2ee2b9ac1870e84af1edaf7a8e9c76a00bb6d3a9
txxxxc <tomoya113.code@gmail.com>
2023-01-28 00:06:29 +0900

    [update] algo-shiki dp q5

--- algo-shiki/dp/q5/index.cpp
+++ algo-shiki/dp/q5/index.cpp
@@ -8,8 +8,17 @@ int main() {
   int N, M;
   cin >> N >> M;
   vector<int> list(N);
-  vector<int> answer(N);
   rep(i, N) cin >> list[i];
-
+  vector<int> answer(N, 1000000);
+  answer[0] = 0;
+  for (int i = 1; i < N; i++) {
+    for (int j = 1; j <= M; j++) {
+      if (i - j < 0)
+        continue;
+      int tmp = answer[i - j] + list[i] * j;
+      answer[i] = min(answer[i], tmp);
+    }
+  }
+  cout << answer[N - 1] << endl;
   return 0;
 }
