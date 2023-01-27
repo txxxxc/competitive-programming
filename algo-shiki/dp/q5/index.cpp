4d7a03313429ee4db3c1ffafd10a8dd02ee8ba1a
txxxxc <tomoya113.code@gmail.com>
2023-01-27 22:45:16 +0900

    [solve] algo-shiki dp q5

---
+++ algo-shiki/dp/q5/index.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(arg) std::cerr << #arg << ": " << (arg) << '\n'
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<int> list(N);
+  vector<int> answer(N);
+  rep(i, N) cin >> list[i];
+
+  return 0;
+}
