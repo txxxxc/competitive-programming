eac46b08f51a0fc16172923432268e02de8ac8cb
txxxxc <tomoya113.code@gmail.com>
2023-03-03 16:30:01 +0900

    [solve] algo-shiki binary-search-tree q3

---
+++ algo-shiki/binary-search-tree/q3/index.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(arg) std::cerr << #arg << ": " << (arg) << '\n'
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+int main() {
+	int N, M; cin >> N >> M;
+	vector<int> a(N); rep(i, N) cin >> a[i];
+	vector<int> b(M); rep(i, M) cin >> b[i];
+	rep(i, M) {
+		int tmp = b[i];
+		int left = 0;
+		int right = N-1;
+		while (right - left > 1) {
+			int mid = (right + left) / 2;
+			if (tmp <= a[mid]) right = mid;
+			if (tmp > a[mid]) left = mid+1;
+		}
+		cout << left << endl;
+	}
+
+	return 0;
+}
+
