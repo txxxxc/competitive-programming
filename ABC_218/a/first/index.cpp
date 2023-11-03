d6b38957bcc7910c69a8ae5c15ab481cdeee9868
txxxxc <tomoya113.code@gmail.com>
2023-11-03 14:58:31 +0900

    solve: ABC 218 A, B

---
+++ ABC_218/a/first/index.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(arg) std::cerr << #arg << ": " << (arg) << '\n'
+#define INF ((1LL<<62)-(1LL<<31)) 
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+using ll = long long;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+int main() {
+	int N; cin >> N;
+	string S; cin >> S;
+	if (S[N-1] == 'x') cout << "No" << endl;
+	else cout << "Yes" << endl;
+
+	return 0;
+}
+
---
+++ ABC_218/b/first/index.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(arg) std::cerr << #arg << ": " << (arg) << '\n'
+#define INF ((1LL<<62)-(1LL<<31)) 
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+using ll = long long;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+vector<string> hoge{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+int main() {
+	rep(i, 26) {
+		int v; cin >> v;
+		cout << hoge[v-1];
+	}
+
+	return 0;
+}
+
