e30a32b8402fd5734ddf267979d5e71e76a3202c
txxxxc <tomoya113.code@gmail.com>
2024-01-21 10:00:35 +0900

    solve: ABC139 D

---
+++ ABC_139/d/first/index.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(arg) std::cerr << #arg << ": " << (arg) << '\n'
+#define INF ((1LL<<62)-(1LL<<31)) 
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+using ll = long long;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+int main() {
+	ll N; cin >> N;
+	N--;
+	cout << (ll)((1 + N) * (N-1+1)) / 2 << endl;
+
+	return 0;
+}
